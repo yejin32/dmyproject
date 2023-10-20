13c2c98d4f74d10c64fe95f9b7b9207d95a0e3e5
yejin32 <yejin@yejin-ui-MacBookPro.local>
2023-10-20 15:56:05 +0900

    Design: 메인페이지 수정

---
+++ src/pages/mainPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import Search from "../components/Search";
+
+const mainPage = () => {
+  return (
+    <div>
+      <Sidebar />
+      <Search />
+    </div>
+  );
+};
+
+export default mainPage;
