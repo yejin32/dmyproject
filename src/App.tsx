6de747ed62dfc7a1c4fb6a45f78f2e48d95f7066
yejin32 <yejin@yejin-ui-MacBookPro.local>
2023-10-20 15:38:09 +0900

    Feat: 페이지 라우팅 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/loginPage";
+import MainPage from "./pages/mainPage";
+import RegisterPage from "./pages/registerPage";
 
-function App() {
+function App(): JSX.Element {
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
